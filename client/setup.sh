93e683142bae3e27df64b5cbae8b328b3a33589c
Raymond Blum <rsygeeknyc@gmail.com>
2019-04-05 10:02:49 -0400

    Add filler zip file

--- client/setup.sh
+++ client/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #echo "infinitecage"
 cmd_dir=`dirname $0`
-echo export GOOGLE_APPLICATION_CREDENTIALS=`ls $cmd_dir/../../infinitecage*json | head -1`
+echo export GOOGLE_APPLICATION_CREDENTIALS=`ls $cmd_dir/../infinitecage*json | head -1`
